87c56b64e6d499a27841e2722f5397b36a837baf
Gabriellji <gabriellji@yahoo.com>
2021-04-07 00:59:55 +0300

    env vars

--- ex1.js
+++ ex1.js
@@ -1,38 +1,60 @@
 #!/usr/bin/env node
 
 "use strict";
+const util = require("util");
+const path = require("path");
+const fs = require("fs");
+
+const getStdin = require("get-stdin");
 
 const args = require("minimist")(process.argv.slice(2), {
-    boolean: [ "help" ],
-    string: [ "file" ]
-})
+  boolean: ["help", "in"],
+  string: ["file"],
+});
 
-if(args.help) {
-    printHelp();
-} else if (args.file) {
-    console.log(args.file)
-} else {
-    error("incorrect usage.", true)
+const BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
+
+if (process.env.HELLO) {
+  console.log(process.env.HELLO);
 }
 
-
-console.log(args)
+if (args.help) {
+  printHelp();
+} else if (args.in || args._.includes("-")) {
+  getStdin().then(processFile).catch(error);
+} else if (args.file) {
+  fs.readFile(path.join(BASE_PATH, args.file), (err, contents) => {
+    if (err) {
+      error(err.toString());
+    } else {
+      processFile(contents.toString());
+    }
+  });
+} else {
+  error("Incorrect usage.", true);
+}
 
 function error(msg, includeHelp = false) {
-    console.error(msg);
-    if(includeHelp) {
-        console.log("")
-        printHelp()
-    }
+  console.error(msg);
+  if (includeHelp) {
+    console.log("");
+    printHelp();
+  }
 }
 
-/******** */
+/*****************************/
+
+function processFile(contents) {
+  contents = contents.toUpperCase();
+  process.stdout.write(contents);
+}
 
 function printHelp() {
-    console.log("ex1 usage:")
-    console.log("   ex1.js --help")
-    console.log("")
-    console.log("--help                print this help")
-    console.log("--file={FILENAME}      process the file")
-    console.log("")
+  console.log("ex1 usage:");
+  console.log("   ex1.js --help");
+  console.log("");
+  console.log("--help                print this help");
+  console.log("--file={FILENAME}      process the file");
+  console.log("--in, -               process stdin");
+  console.log("");
 }
